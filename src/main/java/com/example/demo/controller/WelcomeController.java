51d9a85bdd65681ec95a85878885b3c51a6a3959
Alicja Reniewicz <32552145+alicjaren@users.noreply.github.com>
2017-12-27 19:50:19 +0100

    Add Spring Security configuration (only 2 build-in users: user and admin)

---
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+//    @Value("${welcome.message:test}")
+//    private String message = "Hello World";
+//
+//    @RequestMapping("/")
+//    public String welcome(Map<String, Object> model){
+//        model.put("message", this.message);
+//        return "welcome";
+//    }
+
+    @GetMapping("/")
+    public String home1() {
+        return "/home";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "/home";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "/user";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/about";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+    @GetMapping("/403")
+    public String error403() {
+        return "/error/403";
+    }
+
+}
